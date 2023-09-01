b111ffde0e4e1f15a60c31eaf0edc0f0330cd71a
Nathan Rice <nathan.rice@teamsparq.com>
2023-09-01 15:40:33 -0500

    added more stuff

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import spacePatches from "../assets/images/Space.jpeg"
 
 export default function Home () {
     return(
-        <div>HOME</div>
+        <div className="home">
+            <div>
+                <h1>SPACE PATCHES IN NOW!</h1>
+            </div>
+            <div>
+                <img src={spacePatches} alt="Space Patches!" className="img-content"/>
+            </div>
+            <div>
+
+            </div>
+        </div>
     )
 }
